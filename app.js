c6b04de96d61715c4dce5fdf270ba9130f831bb4
bheemaguli <sumanth.bhi@gmail.com>
2022-05-23 17:03:04 +0530

    query

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const app = express()
+// req => middleware => res
 
-app.get('/', (req,res)=>{
-    res.json([{name:'Sumanth'}, {name: 'Sudhanva'}])
-})
+
 
 app.listen(5000, ()=>{
     console.log('Server is listining on port 5000...')
