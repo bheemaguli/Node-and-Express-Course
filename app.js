9fee688aef54f35cee82ccceccf8d9b34423267c
bheemaguli <sumanth.bhi@gmail.com>
2022-05-24 18:29:25 +0100

    Update

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
 const express = require('express')
 const app = express()
-// req => middleware => res
 
-const logger = (req, res, next) =>{
-    const method = req.method
-    const url = req.url
-    const time = new Date().getFullYear()
-    console.log(method, url, time)
-    // res.send('testing')
-    next()
-}
+let { people } = require('./data')
 
-app.get('/', logger, (req, res)=>{
-    res.send('Home')
+
+
+
+
+
+app.get('/api/people', (req, res) => {
+    res.status(200).json({success: true, data: people})
 })
 
-app.get('/about', logger, (req, res)=>{
-    res.send('About')
-})
+
 
 app.listen(5000, ()=>{
     console.log('Server is listining on port 5000...')
