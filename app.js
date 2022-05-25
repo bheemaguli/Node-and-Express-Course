9107da46aa5e66222cfe3799c89467e983ab67fd
bheemaguli <sumanth.bhi@gmail.com>
2022-05-25 18:31:20 +0530

    Express

--- app.js
+++ app.js
@@ -1,27 +1,6 @@
 const express = require('express')
 const app = express()
 
-let { people } = require('./data')
-
-//  static asset
-app.use(express.static('./methods-public'))
-
-// parse from data
-app.use(express.urlencoded({extended:false}))
-
-app.get('/api/people', (req, res) => {
-    res.status(200).json({success: true, data: people})
-})
-
-app.post('/login', (req, res) => {
-    const { name } = req.body
-    if (name) {
-        return res.status(200).send(`Welcome ${name}`)
-    }
-    res.status(404).send('Please provide the necessary credentials.')
-})
-
-
 app.listen(5000, ()=>{
     console.log('Server is listining on port 5000...')
 })
