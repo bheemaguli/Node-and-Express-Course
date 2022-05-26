bcd806b62c989c4f5967b4c49385627e26173fb9
bheemaguli <sumanth.bhi@gmail.com>
2022-05-26 16:06:48 +0530

    Project1

--- 03-task-manager/final/app.js
+++ 03-task-manager/final/app.js
@@ -1,1 +1,11 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+
+// routes
+app.get('/hello', (req, res)=>{
+    // console.log(req)
+    res.send('Task Manager App')
+})
+
+const port = 3000
+app. listen(port, console.log(`Server is listening on port ${port}...`))
